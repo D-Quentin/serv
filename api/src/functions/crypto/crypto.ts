859629c37bbfde6c04474d28549f0faf8ef82f23
Quentin <quentin.daville@epitech.eu>
2022-07-16 01:26:42 +0200

    [Fix] remove testApiKey

--- api/src/functions/crypto/crypto.ts
+++ api/src/functions/crypto/crypto.ts
@@ -3,9 +3,6 @@ import { logger } from 'src/lib/logger'
 import { createPosition, closePosition, getPrice } from './BinanceApi'
 import { db } from 'src/lib/db';
 
-const API_KEY = "faf2549fdbba8ad56485f950937a3891381b191ebe96d68d9ac4c3f6457fd339";
-const SECRET_KEY = "36ed2fbf88c7ad8e28497c028a1e4b68322f74677aedf712fab260041cf6c714";
-
 export const handler = async (event: APIGatewayEvent, context: Context) => {
   const body = JSON.parse(event.body)
   const userData = await db.user.findUnique({
